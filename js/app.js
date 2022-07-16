5d944669e038484ad5bfd35a4b033a81e7f104f6
Angela DeLeon <aedeleon2023@gmail.com>
2022-07-16 14:43:22 -0700

    solved problem three'

--- js/app.js
+++ js/app.js
@@ -56,11 +56,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let sum = (a + b + c);
+  let product = (a * b * c);
+  let thirdElement = `${a} and ${b} and ${c} sum to ${sum}.`;
+  let fourthElement = `The product of ${a} and ${b} and ${c} is ${product}.`;
+  return [sum, product, thirdElement, fourthElement];
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+// eslint-disable-next-line no-undef
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -77,9 +82,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
 
-}
 
 // Here is the test for sumArray(); uncomment it to run it
 
@@ -99,8 +102,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
 }
+
 
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
