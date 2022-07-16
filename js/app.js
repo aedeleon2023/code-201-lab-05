3f6bf3d84fc334b4ed634bb09d45bc809d5ffbad
Angela DeLeon <aedeleon2023@gmail.com>
2022-07-15 17:42:35 -0700

    solved problem two'

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ function sum(a, b) { //eslint-disable-line
 }
 
 // Here is the test for sum(); uncomment it to run it
+// eslint-disable-next-line no-undef
 testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
@@ -30,11 +31,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  let product = a * b;
+  let myStr = `The product of ${a} and ${b} is ${product}.`;
 
+  return [product, myStr];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+// eslint-disable-next-line no-undef
+testMultiply(5, 9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
